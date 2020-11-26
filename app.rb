7d9144fceae4308a265319deae37a71a6edc5425
Samara Grant <samara.amanda01@gmail.com>
2020-11-26 08:57:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/show' do
+      erb :show
+    end
 
   end
 end
